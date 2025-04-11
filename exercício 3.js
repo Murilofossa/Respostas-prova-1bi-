6dde0b58df3544a0c911101b88ce98292f1453a0
Murilofossa <murilo.fossa@aluno.ifsp.edu.br>
2025-04-11 16:45:23 -0300

    Update exercício 3.js

--- exercício 3.js
+++ exercício 3.js
@@ -6,14 +6,14 @@ function registrarPedido(cliente, prato, mesa, idade) {
     idade:35
   };
 }
-
+let idade=[]
 // 2. Crie um array vazio chamado pedidos
 let pedido = [];
 
 // 3. Adicione 3 pedidos ao array usando a função criada
-function.splice(registrarPedido("Ana", "Hambúrguer", 1, 10));
-function.splice(registrarPedido("Bruno", "Lasanha", 2, 17));
-function.splice(registrarPedido("Carlos", "Feijoada", 3, 65));
+pedido.splice(registrarPedido("Ana", "Hambúrguer", 1, 10));
+pedido.splice(registrarPedido("Bruno", "Lasanha", 2, 17));
+pedido.splice(registrarPedido("Carlos", "Feijoada", 3, 65));
 
 // 4. Crie uma função chamada classificarIngresso que retorna o tipo com base na idade
 function classificarIngresso() {
@@ -31,31 +31,31 @@ function classificarIngresso() {
 // 5. Atendimento dos pedidos (sem laço de repetição)
 
 // Atendimento do primeiro pedido
-let pedido1 = __________.__________();
-let tipo1 = __________(pedido1.__________);
-let __________ = 29.9;
-console.log("Cliente atendido:", pedido1.__________);
+let pedido1 = pedido.shift();
+let tipo1 = classificarIngresso(pedido1.idade);
+let valor1 = 29.9;
+console.log("Cliente atendido:", pedido1.cliente);
 console.log("Tipo de ingresso:", classificarIngresso);
-console.log("Valor da conta: R$", valor1.__________(2));
-console.log("_________________________");
+console.log("Valor da conta: R$", valor1.toFixed(2));
+console.log("registrarPedido");
 
 // Atendimento do segundo pedido
-let pedido2 = pedidos.__________();
-let tipo2 = classificarIngresso(pedido2.__________);
-let valor2 = __________;
-console.log("Cliente atendido:", pedido2.__________);
+let pedido2 = pedidos.shift();
+let tipo2 = classificarIngresso(pedido2.idade);
+let valor2 = 29.9;
+console.log("Cliente atendido:", pedido2.cliente);
 console.log("Tipo de ingresso:", tipo2);
-console.log("Valor da conta: R$", __________.toFixed(2));
-console.log("_________________________");
+console.log("Valor da conta: R$", valor3.toFixed(2));
+console.log("registrarPedido");
 
 // Atendimento do terceiro pedido
 let pedido3 = pedidos.shift();
-let tipo3 = classificarIngresso(__________.idade);
+let tipo3 = classificarIngresso(pedido3.idade);
 let valor3 = 29.9;
-console.log("Cliente atendido:", __________.cliente);
+console.log("Cliente atendido:", pedido3.cliente);
 console.log("Tipo de ingresso:", tipo3);
-console.log("Valor da conta: R$", valor3.__________(2));
-console.log("_________________________");
+console.log("Valor da conta: R$", valor3.toFixed(2));
+console.log("registrarPedido");
 
 // 6. Exiba a quantidade de pedidos restantes
-console.log("Pedidos restantes:", __________.__________);
+console.log("Pedidos restantes:", pedido);
